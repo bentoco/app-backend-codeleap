39dbe65701116e0a6a2c6ff32c9143a11faee957
bentoco <bentoco@outlook.com.br>
2024-03-17 11:49:14 -0300

    update post model

--- careers/models.py
+++ careers/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=100)
-    created_datetime = models.DateTimeField()
+    created_datetime = models.DateTimeField(auto_now_add=True, auto_now=False, blank=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
 
